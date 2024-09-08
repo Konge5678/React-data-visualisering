7d276fe6e98abae3d5d98c3ddeffeadeb3687c29
KrisH <123541182+Konge5678@users.noreply.github.com>
2024-09-08 20:02:31 +0200

    kanelbolle*****

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,62 @@
-export default function Home() {
-    return (
-        <div>
+import { useEffect, useRef, useState } from "react";
 
-        </div>
-    );
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [page, setPage] = useState(0);
+
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = new AbortController();
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(`${BASE_URL}/posts?page=${page}`, {
+          signal: abortControllerRef.current?.signal,
+        });
+        const posts = (await response.json()) as Post[];
+        setPosts(posts);
+      } catch (e: any) {
+        if (e.name === "AbortError") {
+          console.log("aborted");
+          return;
+        }
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, [page]);
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  return (
+    <div className="tutorial">
+      <h1 className="mb-4 text-2xl"></h1>
+      <button onClick={() => setPage(page + 1)}>Next page ({page})</button>
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && (
+        <ul>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
 }
